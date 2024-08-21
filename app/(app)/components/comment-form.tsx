7045391f28908d055898534122c5ddb63713b197
ngodup <ngodupbz@gmail.com>
2024-08-21 11:26:46 +0200

    Add comment form component

---
+++ app/(app)/components/comment-form.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+interface CommentFormProps {
+  existingComment?: string;
+  onSubmit: (data: { comment: string }) => void;
+  submitLabel?: string;
+  placeholder?: string;
+  className?: string;
+}
+
+export default function CommentForm({
+  existingComment = "",
+  onSubmit,
+  submitLabel = "Submit Comment",
+  placeholder = "Add your comment...",
+  className,
+}: CommentFormProps) {
+  // Use React Hook Form
+  const formMethods = useForm({
+    defaultValues: { comment: existingComment },
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = formMethods;
+
+  // Form submission handler
+  const handleFormSubmit = (data: { comment: string }) => {
+    debugger;
+    onSubmit(data);
+  };
+
+  return (
+    <div className={cn("mt-4 w-full", className)}>
+      <Form {...formMethods}>
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <FormField
+            name="comment"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Comment</FormLabel>
+                <FormControl>
+                  <Textarea
+                    {...register("comment", {
+                      required: "Comment is required",
+                      minLength: {
+                        value: 5,
+                        message: "Comment must be at least 5 characters long",
+                      },
+                    })}
+                    placeholder={placeholder}
+                    className="w-full rounded-md border border-gray-300 p-3 focus:border-blue-500"
+                    rows={4}
+                    {...field} // Connects the form field to React Hook Form
+                  />
+                </FormControl>
+                {errors.comment && (
+                  <FormMessage>{errors.comment.message}</FormMessage>
+                )}
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end">
+            <Button
+              type="submit"
+              className="bg-primary mt-4 text-primary-foreground px-2 py-2 rounded-md text-sm"
+            >
+              {submitLabel}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
--- app/(app)/products/[id]/page.tsx
+++ app/(app)/products/[id]/page.tsx
@@ -4,9 +4,11 @@ import { FoodItem, RootState } from "@/types";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import { useSelector } from "react-redux";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import ProductRating from "../../components/product-rating";
 import ProductQuantities from "../../components/product-quantities";
+import CommentForm from "../../components/comment-form";
+import PageContainer from "@/components/layout/page-container";
 
 type Props = {
   params: { id: string };
@@ -22,48 +24,59 @@ export default function ProductDetail({ params }: Props) {
     notFound();
   }
 
+  const handleCommentSubmit = () => {
+    console.log("click form");
+  };
+
   return (
-    <Card className="m-10">
-      <CardContent className="p-4 flex flex-wrap md:flex-nowrap gap-2">
-        <div className="w-full md:w-1/2">
-          <Image
-            src={`/assets/images/${food.imageName}`}
-            alt={food.name}
-            width={270}
-            height={200}
-            className="w-full h-auto rounded-xl"
-          />
-        </div>
-        <div className="w-full md:w-1/2">
-          <h3 className="font-semibold text-lg">{food.name}</h3>
-          <div className="mb-4">
-            <p className="text-sm">
-              <span className="font-semibold">Description: </span>
-              La quiche lorraine est une variante de quiche, une tarte salée de
-              la cuisine lorraine et de la cuisine française, à base de pâte
-              brisée ou de pâte feuilletée, de migaine et de lardons.
-            </p>
+    <PageContainer scrollable={true}>
+      <Card className="m-10">
+        <CardContent className="p-4 flex flex-wrap md:flex-nowrap gap-2">
+          <div className="w-full md:w-1/2">
+            <Image
+              src={`/assets/images/${food.imageName}`}
+              alt={food.name}
+              width={270}
+              height={200}
+              className="w-full h-auto rounded-xl"
+            />
           </div>
-          <div className="flex justify-between">
-            <p>
-              <span className="font-semibold">Prix :</span> €{food.price}
-            </p>
-            <ProductRating rating={food.rating} />
+          <div className="w-full md:w-1/2">
+            <h3 className="font-semibold text-lg">{food.name}</h3>
+            <div className="mb-4">
+              <p className="text-sm">
+                <span className="font-semibold">Description: </span>
+                La quiche lorraine est une variante de quiche, une tarte salée
+                de la cuisine lorraine et de la cuisine française, à base de
+                pâte brisée ou de pâte feuilletée, de migaine et de lardons.
+              </p>
+            </div>
+            <div className="flex justify-between">
+              <p>
+                <span className="font-semibold">Prix :</span> €{food.price}
+              </p>
+              <ProductRating rating={food.rating} />
+            </div>
+            {/* Quantity component */}
+            <ProductQuantities className="mt-5" />
+            {/* Food menu category */}
+            <div className="flex gap-4 mt-5 text-sm">
+              <Card className="bg-blue-700 text-primary-foreground p-2">
+                {food.category}
+              </Card>
+              <Card className="bg-primary text-primary-foreground p-2">
+                {food.repas}
+              </Card>
+              <Card className="bg-lightGreen text-black p-2">
+                {food.repasType}
+              </Card>
+            </div>
           </div>
-          {/* Quantity component */}
-          <ProductQuantities className="mt-5" />
-          {/* Food menu category */}
-          <div className="flex gap-4 mt-5 text-sm">
-            <Card className="bg-blue-700 text-primary-foreground p-2">
-              {food.category}
-            </Card>
-            <Card className="bg-primary text-primary-foreground p-2">
-              {food.repas}
-            </Card>
-            <Card className="bg-lightGreen p-2">{food.repasType}</Card>
-          </div>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+        <CardFooter>
+          <CommentForm className="w-full" onSubmit={handleCommentSubmit} />
+        </CardFooter>
+      </Card>
+    </PageContainer>
   );
 }
